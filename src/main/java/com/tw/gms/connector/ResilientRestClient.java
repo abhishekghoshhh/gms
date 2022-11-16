75e79ea0d89ea06ae92bc34d511569e41daa1f13
abhishekghosh <abhishek.ghosh@thoughtworks.com>
2022-11-16 17:22:01 +0530

    GMS API facade | Rajat,Abhishek,Hari | Implement logger with fixed pattern

--- src/main/java/com/tw/gms/connector/ResilientRestClient.java
+++ src/main/java/com/tw/gms/connector/ResilientRestClient.java
@@ -1,22 +1,22 @@
 package com.tw.gms.connector;
 
+import org.apache.logging.log4j.ThreadContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
 import java.net.URI;
-import java.util.concurrent.TimeoutException;
+import java.util.Map;
 
 @Service
 public class ResilientRestClient {
@@ -25,6 +25,8 @@ public class ResilientRestClient {
 
     @Autowired
     CircuitBreakerFactory circuitBreakerFactory;
+
+    Logger log = LoggerFactory.getLogger(ResilientRestClient.class);
 
     public ResilientRestClient(@Value("${template.withSsl}") String restTemplateType,
                                @Autowired RestTemplate templateWithSSL,
@@ -38,17 +40,27 @@ public class ResilientRestClient {
 
     public <T> ResponseEntity<T> exchange(String hystrixKey, URI url, HttpMethod method, HttpEntity<?> httpEntity, Class<T> type, T defaultResponse) {
         CircuitBreaker circuitBreaker = getCircuitBreaker(hystrixKey);
+        Map<String, String> contextMap = getContextMap();
         return circuitBreaker.run(() -> {
+            MDC.setContextMap(contextMap);
+            log.debug("calling to {} ", url);
+            log.debug("headers are {}", httpEntity.getHeaders());
             return restTemplate.exchange(url, method, httpEntity, type);
         }, throwable -> {
+            MDC.setContextMap(contextMap);
+            log.debug("returning the default response");
             return ResponseEntity.ok(defaultResponse);
         });
     }
 
     public <T> ResponseEntity<T> exchange(String hystrixKey, URI url, HttpMethod method, HttpEntity<?> httpEntity, Class<T> type) throws RestCallException {
         CircuitBreaker circuitBreaker = getCircuitBreaker(hystrixKey);
+        Map<String, String> contextMap = getContextMap();
         try {
             return circuitBreaker.run(() -> {
+                MDC.setContextMap(contextMap);
+                log.debug("calling to {} ", url);
+                log.debug("headers are {}", httpEntity.getHeaders());
                 return restTemplate.exchange(url, method, httpEntity, type);
             }, throwable -> {
                 throw new RuntimeException(throwable);
@@ -58,8 +70,13 @@ public class ResilientRestClient {
         }
     }
 
+    private Map<String, String> getContextMap() {
+        return null != ThreadContext.getContext() ? ThreadContext.getContext() : MDC.getCopyOfContextMap();
+    }
+
     private <T> T buildException(RuntimeException exception) throws RestCallException {
         Throwable cause = exception.getCause();
+        log.error("Error from server is : {}", (null != cause ? cause.getMessage() : exception.getMessage()));
         if (cause instanceof HttpClientErrorException) {
             HttpClientErrorException httpClientErrorException = (HttpClientErrorException) cause;
             throw new RestCallException("Unauthorized Access",
--- src/main/java/com/tw/gms/intercepter/GmsInterceptor.java
+++ src/main/java/com/tw/gms/intercepter/GmsInterceptor.java
@@ -1,6 +1,5 @@
 package com.tw.gms.intercepter;
 
-import org.apache.http.protocol.HttpContext;
 import org.apache.logging.log4j.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Component
@@ -24,27 +23,31 @@ public class GmsInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        log.info("log class name {}", log.getClass().getName());
-        log.info("log class {}", log.getClass());
-        log.info("uuid {}", UUID.randomUUID());
-        log.info("time {}", new SimpleDateFormat("yyyy-MM-dd::HH:mm:ss.SSS").format(new Date()));
-        log.info("uri {}", request.getRequestURI());
-        log.info("method name {}", request.getMethod());
-        log.info("thread context {}", ThreadContext.getContext());
-        log.info("mdc context map {}", MDC.getCopyOfContextMap());
-//        MDC.setContextMap(ThreadContext.getContext());
+        Map<String, String> context = new HashMap<>();
+        context.put("thread-name", Thread.currentThread().getName());
+        context.put("uuid", UUID.randomUUID().toString());
+        context.put("method", request.getMethod());
+        context.put("uri", request.getRequestURI());
+        context.put("startTime", String.valueOf(System.currentTimeMillis()));
+        ThreadContext.putAll(context);
+        MDC.setContextMap(context);
+        log.info("transaction started");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
                            @Nullable ModelAndView modelAndView) throws Exception {
-
-        ThreadContext.clearAll();
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
                                 @Nullable Exception ex) throws Exception {
+        Map<String, String> context = MDC.getCopyOfContextMap();
+        long responseTime = System.currentTimeMillis() - Long.valueOf(context.get("startTime"));
+        log.info("transaction completed");
+        log.info("ResponseCode={}|ResponseTime={}ms", response.getStatus(), responseTime);
+        ThreadContext.clearMap();
+        MDC.clear();
     }
 }
