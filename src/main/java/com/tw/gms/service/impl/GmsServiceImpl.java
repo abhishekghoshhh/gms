7a94732706e969c51e1fb01df2925c6ecb6bc3a0
abhishekghosh <abhishek.ghosh@thoughtworks.com>
2022-11-18 16:37:22 +0530

    GMS API facade | Rajat,Abhishek,Hari | test case added

--- src/main/java/com/tw/gms/service/impl/GmsServiceImpl.java
+++ src/main/java/com/tw/gms/service/impl/GmsServiceImpl.java
@@ -3,6 +3,8 @@ package com.tw.gms.service.impl;
 import com.tw.gms.connector.RestCallException;
 import com.tw.gms.model.ProfileResponse;
 import com.tw.gms.service.GmsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -17,9 +19,10 @@ public class GmsServiceImpl implements GmsService {
     public static final String EMPTY_STRING = "";
     @Autowired
     ProfileFetcher profileFetcher;
-
+    Logger log = LoggerFactory.getLogger(GmsServiceImpl.class);
     @Override
     public String isAMember(String token, List<String> groups) throws RestCallException {
+        log.debug("Incoming groups are {}",groups);
         ProfileResponse profileResponse = profileFetcher.fetch(token);
         if (CollectionUtils.isEmpty(profileResponse.getGroups())) {
             return EMPTY_STRING;
