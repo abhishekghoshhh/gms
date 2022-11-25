c877b7b9a70b259e4821599b7175f915d94c343c
abhishekghosh <abhishek.ghosh@thoughtworks.com>
2022-11-25 16:19:32 +0530

    GMS | Rajat,Abhishek,Hari | added logger + containerization

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:17-jdk-slim
 COPY target/group-management-service-0.0.1.jar application.jar
-EXPOSE 443
+EXPOSE 8080
 ENTRYPOINT ["java","-jar","application.jar","${GMS_JAVA_OPTS}"]
--- src/main/java/com/tw/gms/GroupManagementServiceApplication.java
+++ src/main/java/com/tw/gms/GroupManagementServiceApplication.java
@@ -1,13 +1,20 @@
 package com.tw.gms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroupManagementServiceApplication {
+    private static Logger log = LoggerFactory.getLogger(GroupManagementServiceApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(GroupManagementServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        log.info("arguments specified");
+        for (String arg : args) {
+            log.info("{}", args);
+        }
+        SpringApplication.run(GroupManagementServiceApplication.class, args);
+    }
 
 }
