91d453e55d3acf8ab05c7510ec68a19547d669ec
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2024-03-18 15:40:29 +0530

    modfied config

--- cmd/gms/main.go
+++ cmd/gms/main.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/abhishekghoshhh/gms/internal/api"
+	"github.com/abhishekghoshhh/gms/pkg/config"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	config := config.New()
+	println(config.GetString("app.profiles"))
+	println(config.Get("age"))
 	router := mux.NewRouter()
 	capabilitiesController := api.CapabilitiesController()
 	router.HandleFunc("/capabilities", capabilitiesController.GetTemplate)
---
+++ pkg/logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var log *zap.Logger
+
+func init() {
+	config := zap.NewProductionConfig()
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.StacktraceKey = ""
+	config.EncoderConfig = encoderConfig
+
+	var err error
+	log, err = config.Build(zap.AddCallerSkip(1))
+	// As we are accessing the log variable from these public function so in the logs the caller function will be shown as these Info, debug functions, that's why we are skipping one level
+	if err != nil {
+		panic(err)
+	}
+}
+func Info(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
+}
+
+func Fatal(message string, fields ...zap.Field) {
+	log.Fatal(message, fields...)
+}
+
+func Debug(message string, fields ...zap.Field) {
+	log.Debug(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	log.Error(message, fields...)
+}
