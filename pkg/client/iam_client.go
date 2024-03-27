e13e6265af1224644a63d68caa33cbf12f006552
Anuja Chaitanya <anujachaitanya09@gmail.com>
2024-03-27 15:00:48 +0530

    [Anuja/Abhishek] Added getBearerToken method

--- pkg/client/iam_client.go
+++ pkg/client/iam_client.go
@@ -40,3 +40,41 @@ func (iamClient *IamClient) FetchUser(token string) (*model.IamProfileResponse, 
 	}
 	return httpclient.Parse(response, &model.IamProfileResponse{})
 }
+
+func (iamClient *IamClient) FetchAccessTokenForPasswordGrantFlow(config *model.PasswordGrantConfig) (string, error) {
+	headers := make(map[string]string)
+	headers["grant_type"] = "password"
+	headers["username"] = config.UserName()
+	headers["password"] = config.Password()
+	headers["client_id"] = config.ClientId()
+	headers["client_secret"] = config.ClientSecret()
+
+	return iamClient.getBearerToken(headers)
+}
+
+func (iamClient *IamClient) getBearerToken(headers map[string]string) (string, error) {
+	request, err := iamClient.client.Create("POST", iamClient.iamHost, "/token", headers)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := iamClient.client.Send(request)
+	if err != nil {
+		return "", err
+	}
+
+	parsedResponse, err := httpclient.Parse(resp, &model.ClientTokenResponse{})
+	if err != nil {
+		return "", err
+	}
+
+	if iamClient.isBearerToken(parsedResponse) {
+		return parsedResponse.AccessToken, nil
+	}
+
+	return "", errors.New("unable to retrieve Access Token for flow")
+}
+
+func (iamClient *IamClient) isBearerToken(response *model.ClientTokenResponse) bool {
+	return response != nil && response.AccessToken != ""
+}
